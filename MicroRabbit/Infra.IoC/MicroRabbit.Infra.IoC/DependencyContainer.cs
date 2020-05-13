fdfa7d40dc12603572986ca8972bc87ef4e16717
Ningfeng <1733116199@qq.com>
2020-05-12 22:09:29 -0700

    initial commit of infrasture dependency project

---
+++ MicroRabbit/Infra.IoC/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,13 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>()
+        }
+    }
+}
